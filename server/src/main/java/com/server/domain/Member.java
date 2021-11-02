5e6390b1745d42094bc9e6d4c662a44a7dd1fa7c
lee <koma1416@naver.com>
2021-11-02 15:15:11 +0900

    update source

--- server/src/main/java/com/server/domain/Member.java
+++ server/src/main/java/com/server/domain/Member.java
@@ -1,5 +1,8 @@
 package com.server.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,10 +10,10 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Entity
 public class Member {
-	
+	@Id
 	String id;
 	String password;
 	String name;
-	
 }
