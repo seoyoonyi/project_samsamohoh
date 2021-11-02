86569a515d1adde0bf29d9b70e57ea18f72f6be8
lee <koma1416@naver.com>
2021-11-02 16:21:23 +0900

    update source

--- server/src/main/java/com/server/controller/LoginController.java
+++ server/src/main/java/com/server/controller/LoginController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class LoginController {
 	MemberService memberService;
 
 	@PostMapping("/login")
-	public ResponseCode login(Member member) {
+	public ResponseCode login(@RequestBody Member member) {
 		Optional<Member> option = memberService.getMember(member);
 		ResponseCode rc = new ResponseCode();
 
