d73f73668052feb73e63241779d04d771a2c58cf
seoyoonyi <yiseoyoon@gmail.com>
2021-11-10 23:31:56 +0900

    헤더 퍼블리싱 중

--- client/components/grids/header-layout.js
+++ client/components/grids/header-layout.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from "react";
-import Router from "next/router";
-import TokenStorage from "../../common/token";
+import React, { useEffect, useState } from 'react';
+import Router from 'next/router';
+import TokenStorage from '../../common/token';
 
-import { Button, Radio } from "antd";
-import { Layout } from "antd";
+import { Button, Radio } from 'antd';
+import { Layout } from 'antd';
 
 const Headerlayout = ({ token, getToken }) => {
   const tokenStorage = new TokenStorage();
 
   const { Header, Footer, Sider, Content } = Layout;
   const goToLoginpage = () => {
-    Router.push("/login");
+    Router.push('/login');
   };
   const goToRegisterpage = () => {
-    Router.push("/register");
+    Router.push('/register');
   };
 
   const onLoginOut = (e) => {
@@ -25,27 +25,48 @@ const Headerlayout = ({ token, getToken }) => {
   return (
     <>
       <div className="header-wrap">
-        <Header>
+        <Header className="header">
           <h1 className="logo">
             {/* <img src="../../public/images/logo.png" alt="삼삼오오 로고" /> */}
             {/* 이미지 경로 잡는 방법 src에서 /기입하면 public으로 인식함 */}
             <img src="/images/logo.png" alt="삼삼오오 로고" />
           </h1>
           <nav className="navigation">
-            <Button type="primary">새글쓰기</Button>
-
-            {token ? (
-              <Button type="primary" onClick={onLoginOut}>
-                로그아웃
-              </Button>
-            ) : (
-              <Button type="primary" onClick={goToLoginpage}>
-                로그인
-              </Button>
-            )}
-            <Button type="primary" onClick={goToRegisterpage}>
-              회원가입
-            </Button>
+            <ul>
+              <li>
+                {token ? (
+                  <Button
+                    className="logout"
+                    type="primary"
+                    onClick={onLoginOut}
+                  >
+                    로그아웃
+                  </Button>
+                ) : (
+                  <Button
+                    className="login"
+                    type="primary"
+                    onClick={goToLoginpage}
+                  >
+                    로그인
+                  </Button>
+                )}
+              </li>
+              <li>
+                <Button
+                  className="register"
+                  type="primary"
+                  onClick={goToRegisterpage}
+                >
+                  회원가입
+                </Button>
+              </li>
+              <li>
+                <Button className="new-writing" type="primary">
+                  새글쓰기
+                </Button>
+              </li>
+            </ul>
           </nav>
         </Header>
       </div>
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -10,7 +10,7 @@ const App = ({ Component, pageProps }) => {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="icon" type="image/png" href="../public/images/favicon.png" />
+        <link rel="icon" type="image/png" href="/images/favicon.png" />
         <title>삼삼오오</title>
       </Head>
       <Component {...pageProps} />
