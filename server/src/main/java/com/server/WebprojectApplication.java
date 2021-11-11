b05f1780501fb0c3a79fd9bc2a0ed705258f97ab
lee <koma1416@naver.com>
2021-11-11 20:51:11 +0900

    update source

--- server/src/main/java/com/server/WebprojectApplication.java
+++ server/src/main/java/com/server/WebprojectApplication.java
@@ -1,13 +1,32 @@
 package com.server;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WebprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebprojectApplication.class, args);
+	}
+	
+	@Bean
+	public ServletWebServerFactory serverFactory() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+	
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(8090);
+		return connector;
 	}
 
 }
