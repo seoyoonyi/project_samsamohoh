8e2cec0daf5a3e1fb8a686c05a2adef3adf23245
kikio1101 <sadkop00@daum.net>
2021-11-17 20:41:11 +0900

    add proxy2

--- client/next.config.js
+++ client/next.config.js
@@ -2,14 +2,12 @@ const path = require("path");
 
 module.exports = {
   async rewrites() {
-    if (process.env.NODE_ENV !== "production") {
-      return [
-        {
-          destination: "http://13.59.1.176:8090",
-          source: "/",
-        },
-      ];
-    }
+    return [
+      {
+        destination: "http://13.59.1.176:8090",
+        source: "/",
+      },
+    ];
   },
   sassOptions: {
     includePaths: [path.resolve(__dirname, "./pages")],
