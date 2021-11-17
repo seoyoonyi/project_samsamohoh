650f13aec6acbdd52f54637216625cebcc1c6a95
seoyoonyi <yiseoyoon@gmail.com>
2021-11-18 00:00:41 +0900

    햄버거버튼 메뉴 생성

--- client/components/grids/header-layout.js
+++ client/components/grids/header-layout.js
@@ -26,7 +26,7 @@ const Headerlayout = ({ token, getToken }) => {
             {/* 이미지 경로 잡는 방법 src에서 /기입하면 public으로 인식함 */}
             <img src="/images/logo.png" alt="삼삼오오 로고" />
           </h1>
-          <nav className="navigation">
+          <nav className="pc-navi lg-only md-hidden sm-hidden">
             <ul>
               <li className="user-group">
                 <div className="login">
@@ -53,6 +53,42 @@ const Headerlayout = ({ token, getToken }) => {
               </li>
             </ul>
           </nav>
+
+          <nav className="mob-navi md-only sm-only lg-hidden">
+            <button className="menu-btn">
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+          </nav>
+
+          <aside className="sidebar md-only sm-only lg-hidden">
+            <ul>
+              <li className="user-group">
+                <div className="login">
+                  {token ? (
+                    <Link href="/login">
+                      <a onClick="onLoginOut">로그아웃</a>
+                    </Link>
+                  ) : (
+                    <Link href="/login">
+                      <a>로그인</a>
+                    </Link>
+                  )}
+                </div>
+                <div className="register">
+                  <Link href="/register">
+                    <a>회원가입</a>
+                  </Link>
+                </div>
+              </li>
+              <li className="new-writing">
+                <Link href="/">
+                  <a>새글쓰기</a>
+                </Link>
+              </li>
+            </ul>
+          </aside>
         </Header>
       </div>
     </>
