0a07d15d35a7f0da0b500ab2cd969d9ae677655e
kikio1101 <sadkop00@daum.net>
2021-11-30 15:49:30 +0900

    add login validate check

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Layout } from "antd";
 import { Input, Space, Form } from "antd";
 import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
@@ -22,10 +22,8 @@ const Login = () => {
       password: values.pw,
     };
     console.log("로그인", loginForm);
-
     try {
       //   const loginItem = await fetcher("post", "/login", loginForm);
-
       const loginItem = {
         code: 0,
         message: "로그인 성공",
@@ -35,13 +33,10 @@ const Login = () => {
             "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTYzNjUyNzIyNiwiZXhwIjoxNjM2NTI5MDI2fQ.xFXIqdBVmijGmqaORHaVWnoQbbmZRmi8qYz6mUjy98Y",
         },
       };
-
       console.log("loginItem", loginItem);
-
       if (loginItem.code === 0) {
         tokenStorage.saveToken(loginItem);
         alertInfo(loginItem.message, null, "info");
-
         setTimeout(() => {
           setSubmitLoading(false);
           loginItem && router.push("/");
@@ -51,7 +46,6 @@ const Login = () => {
       }
       // undefined가 로컬스토리지에 저장도지 않도록 반드시 loginItem && 앞에 붙여줘야함
       // JSON.stringify는 token.js에서 하도록 위임
-
       // router.back()을 사용하면 회원가입에서 로그인페이지로 넘어온 경우 다시 회원가입으로 넘기기 때문에  문제가 있음
     } catch (error) {
       // axios에서 에러발생시 처리
@@ -88,6 +82,49 @@ const Login = () => {
     Router.back();
   };
 
+  const validateID = useCallback((_, value) => {
+    if (!value) {
+      return Promise.reject(new Error("아이디는 필수 항목입니다."));
+    }
+    if (/\s/.test(value)) {
+      return Promise.reject(new Error("아이디는 공백을 포함 할 수 없습니다."));
+    }
+
+    if (value.length < 5 || value.length > 10) {
+      return Promise.reject(new Error("아이디는 5 ~ 10자 입니다."));
+    }
+    const regExp = /[^a-zA-Z0-9]/;
+    if (regExp.test(value)) {
+      return Promise.reject(
+        new Error("닉네임은 영문과 숫자만 사용할 수 있습니다.")
+      );
+    }
+    return Promise.resolve();
+  }, []);
+
+  const validatePassword = useCallback((_, value) => {
+    const regExp = /(?=.*\d{1,15})(?=.*[~`!@#$%\^&*()-+=]{1,15})(?=.*[a-z]{1,15})(?=.*[A-Z]{1,15}).{8,15}$/;
+    if (!value) {
+      return Promise.reject(new Error("비밀번호는 필수 항목입니다."));
+    }
+    if (/\s/.test(value)) {
+      return Promise.reject(
+        new Error("비밀번호는 공백을 포함 할 수 없습니다.")
+      );
+    }
+    if (value.length < 8 || value.length > 15) {
+      return Promise.reject(new Error("비밀번호는 8 ~ 15자 입니다."));
+    }
+    if (!regExp.test(value)) {
+      return Promise.reject(
+        new Error(
+          "비밀번호는 영문 소문자, 영문 대문자, 숫자, 특수문자를 모두 포함해야 합니다."
+        )
+      );
+    }
+    return Promise.resolve();
+  }, []);
+
   return (
     <>
       <div id="wrap">
@@ -96,10 +133,10 @@ const Login = () => {
             <Space direction="vertical">
               <h4 className="login-txt">삼삼오오에 오신 것을 환영합니다! </h4>
               <Form onFinish={handleFinish}>
-                <Form.Item name="id">
+                <Form.Item name="id" rules={[{ validator: validateID }]}>
                   <Input placeholder="아이디" prefix={<UserOutlined />} />
                 </Form.Item>
-                <Form.Item name="pw">
+                <Form.Item name="pw" rules={[{ validator: validatePassword }]}>
                   <Input.Password
                     placeholder="비밀번호"
                     iconRender={(visible) =>
