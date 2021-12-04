b809ec8a21286df8abd8d478895088f282fb4856
seoyoonyi <yiseoyoon@gmail.com>
2021-12-04 12:19:40 +0900

    메인페이지 퍼블리싱

--- client/pages/mainpage.tsx
+++ client/pages/mainpage.tsx
@@ -6,7 +6,11 @@ import {
   faFilm,
   faPencilAlt,
   faCloud,
+  faHeart,
+  faThumbsUp,
+  faThumbsDown,
 } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
 const Mainpage = ({ roomLists }) => {
   const { Header, Footer, Sider, Content } = Layout;
@@ -18,80 +22,216 @@ const Mainpage = ({ roomLists }) => {
           <div className="slide">
             <img src="/images/main.png" alt="메인페이지" />
           </div>
-          <div className="room-group">
+          <div className="room-wrap">
             <div className="container">
               <h2 className="room-tit">모임 카테고리</h2>
               <ul className="category-list">
                 <li className="category-box">
+                  <button>
+                    <div className="icon-circle is-active">
+                      <span className="all-icon">All</span>
+                    </div>
+                    <p className="category-tit">전체</p>
+                  </button>
                   {/* flaticon으로 연결해놓은 아이콘  */}
                   {/* main.scss에 import 시킴, 화면상에 flaticon이 나타나지 않아요. */}
                   {/* <i className="fi fi-rr-user"></i>
                           <i className="fi fi-br-arrow-right"></i>
                           <i className="fi fi-sr-book"></i>
                           <i className="fi fi-rr-clean"></i> */}
-                  <div className="icon-circle is-active">
-                    <span className="all-icon">All</span>
-                  </div>
-                  <p className="category-tit">전체</p>
-                </li>
-
-                <li className="category-box">
-                  <div className="icon-circle">
-                    <FontAwesomeIcon
-                      icon={faFootballBall}
-                      className="sport-icon"
-                    />
-                  </div>
-                  <p className="category-tit">운동</p>
                 </li>
                 <li className="category-box">
-                  <div className="icon-circle">
-                    <FontAwesomeIcon icon={faUtensils} className="food-icon" />
-                  </div>
-                  <p className="category-tit">맛집</p>
+                  <button>
+                    <div className="icon-circle">
+                      <FontAwesomeIcon
+                        icon={faFootballBall}
+                        className="sport-icon"
+                      />
+                    </div>
+                    <p className="category-tit">운동</p>
+                  </button>
                 </li>
                 <li className="category-box">
-                  <div className="icon-circle">
-                    <FontAwesomeIcon icon={faFilm} className="movie-icon" />
-                  </div>
-                  <p className="category-tit">영화</p>
+                  <button>
+                    <div className="icon-circle">
+                      <FontAwesomeIcon
+                        icon={faUtensils}
+                        className="food-icon"
+                      />
+                    </div>
+                    <p className="category-tit">맛집</p>
+                  </button>
                 </li>
                 <li className="category-box">
-                  <div className="icon-circle">
-                    <FontAwesomeIcon
-                      icon={faPencilAlt}
-                      className="study-icon"
-                    />
-                  </div>
-                  <p className="category-tit">공부</p>
+                  <button>
+                    <div className="icon-circle">
+                      <FontAwesomeIcon icon={faFilm} className="movie-icon" />
+                    </div>
+                    <p className="category-tit">영화</p>
+                  </button>
                 </li>
                 <li className="category-box">
-                  <div className="icon-circle">
-                    <FontAwesomeIcon icon={faCloud} className="etc-icon" />
-                  </div>
-                  <p className="category-tit">기타</p>
+                  <button>
+                    <div className="icon-circle">
+                      <FontAwesomeIcon
+                        icon={faPencilAlt}
+                        className="study-icon"
+                      />
+                    </div>
+                    <p className="category-tit">공부</p>
+                  </button>
+                </li>
+                <li className="category-box">
+                  <button>
+                    <div className="icon-circle">
+                      <FontAwesomeIcon icon={faCloud} className="etc-icon" />
+                    </div>
+                    <p className="category-tit">기타</p>
+                  </button>
                 </li>
               </ul>
-              <ul>
+              {/* <ul>
                 {roomLists.map((item) => {
                   return (
                     <li key={item.seq}>
                       <div> {item.title}</div>
-                      {/* 제목 */}
                       <div>{item.content}</div>
-                      {/* 내용 */}
                       <div>{item.member.name}</div>
-                      {/* 글쓴이 */}
                       <div>{item.cnt}</div>
-                      {/* 구독수 */}
                       <div>{item.bad}</div>
-                      {/* 싫어요 수 */}
                       <div>{item.good}</div>
-                      {/* 좋아요 수 */}
                     </li>
                   );
                 })}
-              </ul>
+              </ul> */}
+              <div className="room-group">
+                <ul className="room-list">
+                  <li className="room-item">
+                    <Link href="/">
+                      <a className="title">맛집을 나누는 식탁 10회차</a>
+                    </Link>
+                  </li>
+                  <li className="room-item">
+                    <Link href="/">
+                      <a className="member">이재원</a>
+                    </Link>
+                  </li>
+                  <li className="room-item">
+                    <Link href="/">
+                      <a className="content">
+                        요리와 술, 대화를 사랑하는 사람 입니다.<br></br>망원동
+                        골목길에 작은 공간에서 글을 쓰며 모임을 열고 있습니다.
+                      </a>
+                    </Link>
+                  </li>
+                  <li className="room-item">
+                    <button className="cnt is-active">
+                      <FontAwesomeIcon icon={faHeart} className="cnt-icon" />
+                      <span className="txt">9999</span>
+                    </button>
+                    <div className="like-list">
+                      <button className="good is-active">
+                        <FontAwesomeIcon
+                          icon={faThumbsUp}
+                          className="good-icon"
+                        />
+                        <span className="txt">9,3천</span>
+                      </button>
+                      <button className="bad is-active">
+                        <FontAwesomeIcon
+                          icon={faThumbsDown}
+                          className="bad-icon"
+                        />
+                        <span className="txt">싫어요</span>
+                      </button>
+                    </div>
+                  </li>
+                </ul>
+                <ul className="room-list">
+                  <li className="room-item">
+                    <Link href="/">
+                      <a className="title">맛집을 나누는 식탁 10회차</a>
+                    </Link>
+                  </li>
+                  <li className="room-item">
+                    <Link href="/">
+                      <a className="member">이재원</a>
+                    </Link>
+                  </li>
+                  <li className="room-item">
+                    <Link href="/">
+                      <a className="content">
+                        요리와 술, 대화를 사랑하는 사람 입니다.<br></br>망원동
+                        골목길에 작은 공간에서 글을 쓰며 모임을 열고 있습니다.
+                      </a>
+                    </Link>
+                  </li>
+                  <li className="room-item">
+                    <button className="cnt">
+                      <FontAwesomeIcon icon={faHeart} className="cnt-icon" />
+                      <span className="txt">9999</span>
+                    </button>
+                    <div className="like-list">
+                      <button className="good">
+                        <FontAwesomeIcon
+                          icon={faThumbsUp}
+                          className="cnt-icon"
+                        />
+                        <span className="txt">9,3천</span>
+                      </button>
+                      <button className="bad">
+                        <FontAwesomeIcon
+                          icon={faThumbsDown}
+                          className="cnt-icon"
+                        />
+                        <span className="txt">싫어요</span>
+                      </button>
+                    </div>
+                  </li>
+                </ul>
+                <ul className="room-list">
+                  <li className="room-item">
+                    <Link href="/">
+                      <a className="title">맛집을 나누는 식탁 10회차</a>
+                    </Link>
+                  </li>
+                  <li className="room-item">
+                    <Link href="/">
+                      <a className="member">이재원</a>
+                    </Link>
+                  </li>
+                  <li className="room-item">
+                    <Link href="/">
+                      <a className="content">
+                        요리와 술, 대화를 사랑하는 사람 입니다.<br></br>망원동
+                        골목길에 작은 공간에서 글을 쓰며 모임을 열고 있습니다.
+                      </a>
+                    </Link>
+                  </li>
+                  <li className="room-item">
+                    <button className="cnt">
+                      <FontAwesomeIcon icon={faHeart} className="cnt-icon" />
+                      <span className="txt">9999</span>
+                    </button>
+                    <div className="like-list">
+                      <button className="good">
+                        <FontAwesomeIcon
+                          icon={faThumbsUp}
+                          className="cnt-icon"
+                        />
+                        <span className="txt">9,3천</span>
+                      </button>
+                      <button className="bad">
+                        <FontAwesomeIcon
+                          icon={faThumbsDown}
+                          className="cnt-icon"
+                        />
+                        <span className="txt">싫어요</span>
+                      </button>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </Content>
