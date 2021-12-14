9cea44edf450f95778af38b6c62e0a7d5d0da035
seoyoonyi <yiseoyoon@gmail.com>
2021-12-15 00:10:47 +0900

    회원가입/ 로그인 버튼 재조정, 404가로스크롤 조정, 헤드 컨데이너 조정

--- client/components/grids/header-layout.tsx
+++ client/components/grids/header-layout.tsx
@@ -23,78 +23,80 @@ const Headerlayout = () => {
     <>
       <div id="header-wrap">
         <Header className="header">
-          <h1 className="logo">
-            {/* <img src="../../public/images/logo.png" alt="삼삼오오 로고" /> */}
-            {/* 이미지 경로 잡는 방법 src에서 /기입하면 public으로 인식함 */}
-            <Link href="/">
-              <a>
-                <img src="/images/logo.svg" alt="삼삼오오 로고" />
-              </a>
-            </Link>
-          </h1>
-          <nav className="pc-navi lg-only md-hidden sm-hidden">
-            <ul>
-              <li className="user-group">
-                <div className="login">
-                  {token ? (
-                    <Link href="/login">
-                      <a onClick={onLoginOut}>로그아웃</a>
+          <div className="container">
+            <h1 className="logo">
+              {/* <img src="../../public/images/logo.png" alt="삼삼오오 로고" /> */}
+              {/* 이미지 경로 잡는 방법 src에서 /기입하면 public으로 인식함 */}
+              <Link href="/">
+                <a>
+                  <img src="/images/logo.svg" alt="삼삼오오 로고" />
+                </a>
+              </Link>
+            </h1>
+            <nav className="pc-navi lg-only md-hidden sm-hidden">
+              <ul>
+                <li className="user-group">
+                  <div className="login">
+                    {token ? (
+                      <Link href="/login">
+                        <a onClick={onLoginOut}>로그아웃</a>
+                      </Link>
+                    ) : (
+                      <Link href="/login">
+                        <a>로그인</a>
+                      </Link>
+                    )}
+                  </div>
+                  <div className="sign-up">
+                    <Link href="/sign_up">
+                      <a>회원가입</a>
                     </Link>
-                  ) : (
-                    <Link href="/login">
-                      <a>로그인</a>
-                    </Link>
-                  )}
-                </div>
-                <div className="sign-up">
-                  <Link href="/sign_up">
-                    <a>회원가입</a>
+                  </div>
+                </li>
+                <li className="new-writing">
+                  <Link href="/">
+                    <a className="btn-main btn-40">새글쓰기</a>
                   </Link>
-                </div>
-              </li>
-              <li className="new-writing">
-                <Link href="/">
-                  <a className="btn-main btn-40">새글쓰기</a>
-                </Link>
-              </li>
-            </ul>
-          </nav>
+                </li>
+              </ul>
+            </nav>
 
-          <nav className="mob-navi md-only sm-only lg-hidden">
-            <button className="menu-btn">
-              <span></span>
-              <span></span>
-              <span></span>
-            </button>
-          </nav>
+            <nav className="mob-navi md-only sm-only lg-hidden">
+              <button className="menu-btn">
+                <span></span>
+                <span></span>
+                <span></span>
+              </button>
+            </nav>
 
-          <aside className="sidebar md-only sm-only lg-hidden">
-            <ul>
-              <li className="user-group">
-                <div className="login">
-                  {token ? (
-                    <Link href="/login">
-                      <a onClick={onLoginOut}>로그아웃</a>
+            <aside className="sidebar md-only sm-only lg-hidden">
+              <ul>
+                <li className="user-group">
+                  <div className="login">
+                    {token ? (
+                      <Link href="/login">
+                        <a onClick={onLoginOut}>로그아웃</a>
+                      </Link>
+                    ) : (
+                      <Link href="/login">
+                        <a>로그인</a>
+                      </Link>
+                    )}
+                  </div>
+                  <div className="register">
+                    <Link href="/register">
+                      <a>회원가입</a>
                     </Link>
-                  ) : (
-                    <Link href="/login">
-                      <a>로그인</a>
-                    </Link>
-                  )}
-                </div>
-                <div className="register">
-                  <Link href="/register">
-                    <a>회원가입</a>
+                  </div>
+                </li>
+                <li className="new-writing">
+                  <Link href="/">
+                    <a>새글쓰기</a>
                   </Link>
-                </div>
-              </li>
-              <li className="new-writing">
-                <Link href="/">
-                  <a>새글쓰기</a>
-                </Link>
-              </li>
-            </ul>
-          </aside>
+                </li>
+              </ul>
+            </aside>
+          </div>
         </Header>
       </div>
     </>
--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -1,11 +1,11 @@
-import { Layout } from "antd";
-import Headerlayout from "../components/grids/header-layout";
-import Head from "next/head";
-import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Layout } from 'antd';
+import Headerlayout from '../components/grids/header-layout';
+import Head from 'next/head';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import Router, { useRouter } from "next/router";
-import { Row, Col } from "antd";
+import Router, { useRouter } from 'next/router';
+import { Row, Col } from 'antd';
 
 const Error404 = () => {
   const { Header, Footer, Sider, Content } = Layout;
@@ -18,7 +18,7 @@ const Error404 = () => {
 
   const goToHome = (e) => {
     e.preventDefault();
-    router.push("/");
+    router.push('/');
   };
 
   //404 page 가로스크롤 생김
@@ -26,40 +26,41 @@ const Error404 = () => {
     <>
       <div id="wrap">
         <Headerlayout />
-        <div id="error-wrap">
+        <div className="error-wrap">
           <Content className="error">
-            <Row gutter={[16, 16]}>
-              <Col span={12}>
-                <div className="error-con-group">
-                  <div className="error-icon-group">
-                    <FontAwesomeIcon
-                      icon={faExclamationCircle}
-                      color="#f3807c"
-                      className="error-icon"
-                    />
-                    <h2 className="error-icon-404">404</h2>
-                  </div>
-
-                  <h2 className="error-tit">
-                    요청하신 페이지를 찾을 수 없습니다.
-                  </h2>
-                  <p className="error-desc">
-                    불편을 드려 죄송합니다.<br></br>확인 후 다시 시도해주세요.
-                  </p>
-                  <div className="error-btn">
-                    <button
-                      onClick={goToHome}
-                      className="error-home-btn btn-main btn-40"
-                    >
-                      홈으로 가기
-                    </button>
-                    <button onClick={goToBack} className="btn-outlined btn-40">
-                      뒤로가기
-                    </button>
-                  </div>
+            <div className="container">
+              <div className="error-con-group">
+                <div className="error-icon-group">
+                  <FontAwesomeIcon
+                    icon={faExclamationCircle}
+                    color="#f3807c"
+                    className="error-icon"
+                  />
+                  <h2 className="error-icon-404">404</h2>
                 </div>
-              </Col>
-            </Row>
+
+                <h2 className="error-tit">
+                  요청하신 페이지를 찾을 수 없습니다.
+                </h2>
+                <p className="error-desc">
+                  불편을 드려 죄송합니다.<br></br>확인 후 다시 시도해주세요.
+                </p>
+                <div className="error-btn">
+                  <button
+                    onClick={goToHome}
+                    className="error-home-btn btn-main btn-40"
+                  >
+                    홈으로 가기
+                  </button>
+                  <button
+                    onClick={goToBack}
+                    className="gray-btn-outlined btn-40"
+                  >
+                    뒤로가기
+                  </button>
+                </div>
+              </div>
+            </div>
           </Content>
         </div>
       </div>
