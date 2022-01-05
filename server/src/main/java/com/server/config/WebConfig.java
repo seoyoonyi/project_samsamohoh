0fef1df61b82b31deb1c8a25b26ee636b0292561
lee <koma1416@naver.com>
2022-01-05 22:52:21 +0900

    jwt토큰v1

--- server/src/main/java/com/server/config/WebConfig.java
+++ server/src/main/java/com/server/config/WebConfig.java
@@ -7,6 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+		registry.addMapping("/**").allowedOrigins("*");
 	}
 }
