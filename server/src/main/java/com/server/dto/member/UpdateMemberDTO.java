1a38e713d3bb0f33ac1778b2300f706d95db1cb9
lee <koma1416@naver.com>
2022-01-19 10:28:30 +0900

    updateMemberDTO 수정

--- server/src/main/java/com/server/dto/member/UpdateMemberDTO.java
+++ server/src/main/java/com/server/dto/member/UpdateMemberDTO.java
@@ -15,13 +15,13 @@ import lombok.Setter;
 public class UpdateMemberDTO {
 	
 	@ApiModelProperty(notes="이름")
-	@Pattern(regexp = "^[가-힣]{2,8}$", message = "이름은 2글자이상 8글자이하의 자음과 모음이 갖춰진 한글만 가능합니다.")
+	@Pattern(regexp = "^[가-힣]{2,8}$", message = "닉네임은 2글자이상 8글자이하의 자음과 모음이 갖춰진 한글만 가능합니다.")
 	private String nickName;
 	
-	@ApiModelProperty(notes="이메일")
+	/*@ApiModelProperty(notes="이메일")
 	@Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "이메일 형식이 올바르지 않습니다.")
 	private String email;
-	
+	*/
 	/*public  Member toEntity() {
 		return Member.builder()
 				.nickName(name)
